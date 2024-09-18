e94413a2955ec3639d501e04d4b383f7096c9d6c
MtcLeo05 <leonardo.matteucci1805@gmail.com>
2024-09-18 10:28:29 +0200

    munchdew radius fix [1.0.8.1]
    Fixed a bug with the munchdew radius

--- src/main/java/com/mtcleo05/botania_editor/mixin/MunchdewMixin.java
+++ src/main/java/com/mtcleo05/botania_editor/mixin/MunchdewMixin.java
@@ -2,14 +2,23 @@ package com.mtcleo05.botania_editor.mixin;
 
 import com.mtcleo05.botania_editor.config.server.GeneratingFloraConfig;
 import net.minecraft.core.BlockPos;
+import net.minecraft.world.level.block.entity.BlockEntityType;
+import net.minecraft.world.level.block.state.BlockState;
+import org.jetbrains.annotations.Nullable;
 import org.spongepowered.asm.mixin.Mixin;
+import org.spongepowered.asm.mixin.Shadow;
 import org.spongepowered.asm.mixin.injection.At;
 import org.spongepowered.asm.mixin.injection.ModifyArg;
 import org.spongepowered.asm.mixin.injection.Redirect;
+import vazkii.botania.api.block_entity.GeneratingFlowerBlockEntity;
+import vazkii.botania.api.block_entity.RadiusDescriptor;
 import vazkii.botania.common.block.flower.generating.MunchdewBlockEntity;
 
 @Mixin(MunchdewBlockEntity.class)
-public class MunchdewMixin {
+public class MunchdewMixin extends GeneratingFlowerBlockEntity {
+    public MunchdewMixin(BlockEntityType<?> type, BlockPos pos, BlockState state) {
+        super(type, pos, state);
+    }
 
     @ModifyArg(remap = false, method = "tickFlower", at = @At(value = "INVOKE", target = "Lvazkii/botania/common/block/flower/generating/MunchdewBlockEntity;addMana(I)V"))
     public int changeManaLeaf(int par1){
@@ -18,17 +27,34 @@ public class MunchdewMixin {
 
     @Redirect(method = "tickFlower", at = @At(value = "INVOKE", target = "Lnet/minecraft/core/BlockPos;betweenClosed(Lnet/minecraft/core/BlockPos;Lnet/minecraft/core/BlockPos;)Ljava/lang/Iterable;"))
     public Iterable<BlockPos> changeRange(BlockPos pFirstPos, BlockPos pSecondPos){
+        BlockPos pos = this.getEffectivePos();
+
         return BlockPos.betweenClosed(
-            new BlockPos(
+            pos.offset(
                 -GeneratingFloraConfig.MUNCHDEW_RANGE.get(),
                 -GeneratingFloraConfig.MUNCHDEW_RANGE_Y.get(),
                 -GeneratingFloraConfig.MUNCHDEW_RANGE.get()
             ),
-            new BlockPos(
+            pos.offset(
                 GeneratingFloraConfig.MUNCHDEW_RANGE.get(),
                 GeneratingFloraConfig.MUNCHDEW_RANGE_Y.get(),
                 GeneratingFloraConfig.MUNCHDEW_RANGE.get()
             )
         );
+    }
+
+    @Shadow(remap = false)
+    public int getMaxMana() {
+        return 0;
+    }
+
+    @Shadow(remap = false)
+    public int getColor() {
+        return 0;
+    }
+
+    @Shadow(remap = false)
+    public @Nullable RadiusDescriptor getRadius() {
+        return null;
     }
 }
